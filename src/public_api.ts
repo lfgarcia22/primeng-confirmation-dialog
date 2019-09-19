4d8c284273dded86462268e91a4ba77b76426e24
Luis García Castro <luis.garcia@von-ds.com>
2019-09-18 20:27:34 -0500

    Library configuration

---
+++ src/public_api.ts
@@ -0,0 +1,4 @@
+export * from './confirmation-dialog.module';
+export * from './lib/confirmation-dialog.config-constants';
+export * from './lib/confirmation-dialog.config-model';
+export * from './lib/confirmation-dialog.service';
